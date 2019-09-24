2d5fc57e870160ec4d04919097850c18d77d79a9
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-09-23 21:36:28 -0300

    tarefa: criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Grazi' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Tammy Fagá',
+    email: 'tammy@gmail.com',
+    password_hash: '65465465',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
