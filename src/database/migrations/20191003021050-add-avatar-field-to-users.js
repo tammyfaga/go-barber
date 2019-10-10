1d46c9746a59e9a1e682177a9b2316d71a867daa
Tammy Fagá <tammyfaga@yahoo.com.br>
2019-10-09 22:50:31 -0300

    tarefa: avatar do usuário

--- src/database/migrations/20191003021050-add-avatar-field-to-users.js
+++ src/database/migrations/20191003021050-add-avatar-field-to-users.js
@@ -1,11 +1,15 @@
-// 'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
 
-// module.exports = {
-//   up: (queryInterface, Sequelize) => {
-//     return
-//   },
-
-//   down: (queryInterface, Sequelize) => {
-
-//   }
-// };
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
